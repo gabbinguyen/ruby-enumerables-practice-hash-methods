939fbd59aaaffe8661948356c8992caa1dcee4bd
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-10-12 21:12:00 -0500

    Done.

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,5 +9,14 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  #code your solution here!
+  groceries.keys
 end
+
+# groceries = {
+#  dairy: ["milk", "yogurt", "cheese"],
+#  vegetable: ["carrots", "broccoli", "cucumbers"],
+#  meat: ["chicken", "steak", "salmon"],
+#  grains: ["rice", "pasta"] }
+
+# binding.pry
+# 0
